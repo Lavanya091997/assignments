60ce2280112c6e546f37c7f96199db012ea91ea6
lavanya091997 <lavanyahsarma09997@gmail.com>
2021-05-10 10:00:27 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+
+document.onload(()=>{
+    searchButtonClicked();
+})
+
+function searchButtonClicked(){
+    var inputValue = document.getElementById("searchInput").value
+    getData(inputValue);
+
+}
+function getData(username){
+
+    var httpRequest = new XMLHttpRequest();
+
+    httpRequest.onload = onDataLoad;
+
+    httpRequest.open("GET", "https://api.github.com/search/users?q=%22" + username +"%22");
+
+    httpRequest.send();
+}
+function onDataLoad(){
+
+        var jsonResponse = JSON.parse(this.response)
+        console.log(jsonResponse);
+        populateUserView(jsonResponse.items)
+
+
+}
+
+
+function populateUserView(users){
+
+    $("#usersList").html("");
+    for(var i = 0; i < users.length-1 ; i++){
+
+        $("#usersList").append(
+            $(getUserView(users[i])).click(()=>{openUrl(users[i].html_url)})
+            );
+
+    }
+
+}
+
+function getUserView(user){
+    return "<div class='user-view'><img src=' " + user.avatar_url + "'><p> " + user.login + "</p></div>";
+}
+
+function openUrl(url){
+    console.log(url);
+    window.open(url);
+}
+
+
+
